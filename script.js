7a94bcecb1a7972cdcac003636d48e6a0362919d
Upendra Kumar Sharma <148332056+Upendra8420@users.noreply.github.com>
2026-09-07 12:53:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-const byteSize = (str) => {
-  // write your code here
-};
-
-// Do not change the code below
-const str = prompt("Enter some string.");
-alert(byteSize(str));
+function byteSize(str) {
+  return new Blob([str]).size;
+}
